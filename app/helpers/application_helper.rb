d026f8dd7a7709b0cde04f1b9f51050b29a56601
Nirmal <nirmalkumar91@gmail.com>
2020-05-28 20:27:34 +0530

    added user detalis in show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,9 @@
 module ApplicationHelper
-    def gravatar_for(user)
+    def gravatar_for(user, option = { size: 80})
         email_address = user.email.downcase
         hash = Digest::MD5.hexdigest(email_address)
-        image_src = "https://www.gravatar.com/avatar/#{hash}?s=200"
+        size = option[:size]
+        image_src = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
         image_tag( image_src, alt: user.username, class: "rounded shadow mx-auto d-block" )
     end
 end
